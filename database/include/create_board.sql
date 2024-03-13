43d5a09b85d4185d0851beac5616f6ceb27b1c40
Qwertst <aydreqstd@gmail.com>
2024-03-13 16:45:52 +0300

    merged database

---
+++ database/include/create_board.sql
@@ -0,0 +1,55 @@
+CREATE TABLE board_signature(
+                                board_id serial PRIMARY KEY,
+                                name varchar(50)
+);
+
+CREATE TABLE list_signature(
+                               list_id serial PRIMARY KEY,
+                               board_id integer REFERENCES board_signature ON DELETE CASCADE,
+                               name varchar(50),
+                               description text
+);
+
+CREATE TABLE card_signature(
+                               card_id serial PRIMARY KEY,
+                               list_id integer REFERENCES list_signature ON DELETE CASCADE,
+                               name varchar(50),
+                               description text
+);
+
+CREATE TABLE tag_signature(
+                              tag_id serial PRIMARY KEY,
+                              name varchar(50)
+);
+
+CREATE TABLE card_to_tags(
+                             card_id integer REFERENCES card_signature ON DELETE CASCADE,
+                             tag_id integer REFERENCES tag_signature ON DELETE CASCADE,
+                             PRIMARY KEY(card_id, tag_id)
+);
+
+CREATE FUNCTION insert_board(name_ varchar(50)) RETURNS
+    void AS $$
+INSERT INTO board_signature VALUES (DEFAULT, name_);
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION insert_list(board_id_ integer, name_ varchar(50),
+                            description_ text) RETURNS void AS $$
+INSERT INTO list_signature VALUES (DEFAULT, board_id_, name_,
+description_);
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION insert_card(list_id_ integer, name_ varchar(50),
+                            description_ text) RETURNS void AS $$
+INSERT INTO card_signature VALUES (DEFAULT, list_id_, name_,
+description_);
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION insert_tag(name_ varchar(50)) RETURNS void AS $$
+INSERT INTO tag_signature VALUES (DEFAULT, name_);
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION insert_into_card_to_tags(card_id_ integer, tag_id_
+    integer) RETURNS void AS $$
+INSERT INTO card_to_tags VALUES (card_id_, tag_id_);
+$$ LANGUAGE SQL;
