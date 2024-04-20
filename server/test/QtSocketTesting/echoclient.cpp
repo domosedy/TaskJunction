23f316c9542536b7b393f0499c3a0e459e587593
domosedy <ilyavin05@mail.ru>
2024-04-20 21:20:52 +0300

    Added program for testing requests without gui

---
+++ server/test/QtSocketTesting/echoclient.cpp
@@ -0,0 +1,41 @@
+// Copyright (C) 2016 Kurt Pattyn <pattyn.kurt@gmail.com>.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+#include "echoclient.h"
+#include <QtCore/QDebug>
+
+QT_USE_NAMESPACE
+
+//! [constructor]
+EchoClient::EchoClient(const QUrl &url, bool debug, QObject *parent) :
+    QObject(parent),
+    m_debug(debug)
+{
+    if (m_debug)
+        qDebug() << "WebSocket server:" << url;
+    connect(&m_webSocket, &QWebSocket::connected, this, &EchoClient::onConnected);
+    connect(&m_webSocket, &QWebSocket::textMessageReceived, this, &EchoClient::onTextMessageReceived);
+    // connect(&m_webSocket, &QWebSocket::disconnected, this, &EchoClient::closed);
+    m_webSocket.open(url);
+}
+//! [constructor]
+
+void EchoClient::sendData(const QString &data) {
+    m_webSocket.sendTextMessage(data);
+}
+
+EchoClient::~EchoClient() {
+    m_webSocket.close();
+}
+//! [onConnected]
+void EchoClient::onConnected() {
+    sendData("{ \"type\": \"login\", \"password\": \"12\", \"user-name\": \"as\"}");
+    sendData(R"({"type": "update", "board-id": 1, "list-id": 2, "card-id": 3, "id": 2, "new-value": "aboba", "field": "name", "object-type": "board"})");
+    sendData("{ \"type\": \"get-boards-info\", \"id\": 1, \"object-type\": \"list\", \"name\": \"52\", \"description\": \"42\"}");
+}
+//! [onConnected]
+
+//! [onTextMessageReceived]
+void EchoClient::onTextMessageReceived(QString message) {
+    qDebug() << "Message received:" << message;
+}
+//! [onTextMessageReceived]
