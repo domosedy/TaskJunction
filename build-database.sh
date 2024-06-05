a4f0400aef3f19e2a670f07c0c7ce0764097a91d
Qwertst <aydreq@mail.ru>
2024-06-06 00:12:52 +0300

    prettify script

--- build-database.sh
+++ build-database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # In honor of Aleksey Zubakov
 
-if [ -f "config" ]; then
+if [[ -f "config" ]]; then
 
     create_path="./database/include/create_board.sql"
     args=()
@@ -13,56 +13,56 @@ if [ -f "config" ]; then
         echo "Loaded $var_name with value: $var_value"
     done < "config"
 
-    if [[ $# -eq 0 ]] ; then
+    if [[ $# -eq 0 ]]; then
         echo 'Provide build options please!'
         exit 0
     fi
 
     if psql -U postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='${args[0]}'" | grep -q 1; then
         echo The user ${args[0]} already exists!
-        else
+    else
         sudo -u postgres psql -c "CREATE USER ${args[0]} WITH PASSWORD '${args[1]}' CREATEDB;"
     fi
 
     while [[ $# -gt 0 ]]; do
-    case $1 in
-        -c|--client)
-        if psql -U postgres -lqt | cut -d \| -f 1 | grep -qw ${args[2]}; then
-            echo "${args[2]} already exists!";
-        else
-            sudo -u postgres psql -c "CREATE DATABASE ${args[2]} OWNER ${args[0]};"
-            sudo -u postgres psql --username=${args[0]} ${args[2]} < $create_path
-        fi
-        shift
-        ;;
-        -s|--server)
-        if psql -U postgres -lqt | cut -d \| -f 1 | grep -qw ${args[3]}; then
-            echo "${args[3]} already exists!";
-        else
-            sudo -u postgres psql -c "CREATE DATABASE ${args[3]} OWNER ${args[0]};"
-            sudo -u postgres psql --username=${args[0]} ${args[3]} < $create_path
-        fi
-        shift
-        ;;
-        -a|--all)
-        if psql -U postgres -lqt | cut -d \| -f 1 | grep -qw ${args[2]}; then
-            echo "${args[2]} already exists!";
-        else
-            sudo -u postgres psql -c "CREATE DATABASE ${args[2]} OWNER ${args[0]};"
-            sudo -u postgres psql --username=${args[0]} ${args[2]} < $create_path
-        fi
-        if psql -U postgres -lqt | cut -d \| -f 1 | grep -qw ${args[3]}; then
-            echo "${args[3]} already exists!";
-        else
-            sudo -u postgres psql -c "CREATE DATABASE ${args[3]} OWNER ${args[0]};"
-            sudo -u postgres psql --username=${args[0]} ${args[3]} < $create_path
-        fi        
-        shift
-        ;;
-        -*|--*)
-        echo "Unknown option $1"
-        exit 1
-    esac
+        case $1 in
+            -c|--client)
+                if psql -U postgres -lqt | cut -d \| -f 1 | grep -qw ${args[2]}; then
+                    echo "${args[2]} already exists!";
+                else
+                    sudo -u postgres psql -c "CREATE DATABASE ${args[2]} OWNER ${args[0]};"
+                    sudo -u postgres psql --username=${args[0]} ${args[2]} < $create_path
+                fi
+                shift
+                ;;
+            -s|--server)
+                if psql -U postgres -lqt | cut -d \| -f 1 | grep -qw ${args[3]}; then
+                    echo "${args[3]} already exists!";
+                else
+                    sudo -u postgres psql -c "CREATE DATABASE ${args[3]} OWNER ${args[0]};"
+                    sudo -u postgres psql --username=${args[0]} ${args[3]} < $create_path
+                fi
+                shift
+                ;;
+            -a|--all)
+                if psql -U postgres -lqt | cut -d \| -f 1 | grep -qw ${args[2]}; then
+                    echo "${args[2]} already exists!";
+                else
+                    sudo -u postgres psql -c "CREATE DATABASE ${args[2]} OWNER ${args[0]};"
+                    sudo -u postgres psql --username=${args[0]} ${args[2]} < $create_path
+                fi
+                if psql -U postgres -lqt | cut -d \| -f 1 | grep -qw ${args[3]}; then
+                    echo "${args[3]} already exists!";
+                else
+                    sudo -u postgres psql -c "CREATE DATABASE ${args[3]} OWNER ${args[0]};"
+                    sudo -u postgres psql --username=${args[0]} ${args[3]} < $create_path
+                fi
+                shift
+                ;;
+            -*|--*)
+                echo "Unknown option $1"
+                exit 1
+        esac
     done
 else
     echo "File 'config' not found."
