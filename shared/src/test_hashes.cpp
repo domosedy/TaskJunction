8f5f1e9fcbe80a036fb70b3c7ee4e5151eebab17
domosedy <ilyavin05@mail.ru>
2024-06-06 21:11:21 +0300

    Deleting server from master

--- shared/src/test_hashes.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "hashes.hpp"
-
-int main() {
-    std::cout << generate_salt().toStdString() << std::endl;
-    std::cout << hash_string("qwerty12345", generate_salt()).toStdString()
-              << std::endl;
-}
